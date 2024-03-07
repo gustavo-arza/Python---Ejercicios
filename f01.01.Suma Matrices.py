a1914220cdf7aad9bfa6e2a811db8cf91bda94dc
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-07 11:19:57 -0300

    Add files via upload

---
+++ f01.01.Suma Matrices.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#extraido del libro Algoritmos resueltos con Python | p217
+#nombre: f01.01.Suma Matrices.py
+print("-------------------------------------------------------")
+print("Matriz1: SUMA DE MATRICES.")
+print("-------------------------------------------------------")
+
+#Inicializar
+A = []
+B = []
+C = []
+
+#Entradas
+print ("Ingrese dimensión de la matriz,máximo 100")
+S = int( input("Número de Filas: "))
+R = int( input("Número Columnas: "))
+
+#Proceso
+for i in range(S):
+A.append( [] ) #Agregamos una i fila vacía en A
+B.append( [] ) #Agregamos una i fila vacía en B
+C.append( [] ) #Agregamos una i fila vacía en C
+for j in range(R):
+A[i].append( int( input("A{}{}: ".format(i+1,j+1))))
+B[i].append( int( input("B{}{}: ".format(i+1,j+1))))
+C[i].append( A[i][j] + B[i][j])
+
+#Salida
+print("\nSALIDA: ")
+print("-------------------------------------------------------")
+print(A)
+print(B)
+print(C)
