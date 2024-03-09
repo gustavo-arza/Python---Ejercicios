8b24b30942f9e0c61c91caa1fbf26ec9d2ffb44a
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-09 06:43:49 -0300

    Add files via upload

---
+++ f01.C4.Vector y Opciones.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#extraido del libro Algoritmos resueltos con Python | p235-337
+#nombre: f01.C4.Vector y Opciones.py
+print("-------------------------------------------------------")
+print("Complemento4: INTERACTUAR CON UN VECTOR A TRAVEZ DE OPC
+IONES.")
+print("-------------------------------------------------------")
+#Inicializar
+V = 100*[0] #Inicializamos un vector con 100 elementos por defecto
+i = 0
+#Entradas
+print ("Ingrese tamaño del vector")
+n = int( input())
+opc = -1
+while opc != 0:
+print("\n-------------------------------------------------------")
+print("Ingrese 1 para añadir un elemento al vector")
+print("Ingrese 2 para eliminar un elemento del vector")
+print("Ingrese 3 para listar el contenido del vector ")
+print("Ingrese 4 para contar las apariciones de Un número en el vector")
+print("Ingrese 5 para calcular la media y el máximo de los elementos de un vector")
+print("Ingrese 0 para terminar")
+
+#Leer Opcion
+opc = int( input("Ingrese Opción: "))
+
+#print("\n")
+if opc == 1:
+if (i < n) :
+
+#Agrega en la posicion 0 y luego incrementa i
+V[i] = int( input("IngreseEntero: "))
+i = i + 1
+elif opc == 2:
+print("Ingrese el número que desea eliminar")
+num = int( input())
+if num > 0:
+a = 0
+#Busca NUM en el array
+for j in range(i):
+if V[j] == num :
+a = j
+break
+#Re-organizar el array
+if a >= 0 and a <= i:
+for j in range(a, i-1 +1):
+V[j] = V[j+1]
+V[i] = 0
+i = i - 1
+elif opc == 3:
+if i > 0:
+for j in range(i):
+print(V[j])
+elif opc == 4:
+c = 0
+print("Ingrese numero para contar número de apariciones: ")
+num = int( input())
+for j in range(i):
+if num == V[j] :
+c = c + 1
+print(" El número de apariciones es: ", c)
+elif opc == 5:
+max = V[0]
+ma = 0
+for j in range(i):
+if max < V[j]:
+max = V[j]
+ma = ma + V[j]
+ma = ma/i
+print("El maximo es:", max, " y la media es: ", ma)
+elif opc == 0:
+print("FIN DE PROGRAMA")
+break
---
+++ f01.C5.Vector y Busqueda.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#extraido del libro Algoritmos resueltos con Python | p235-337
+#nombre: f01.C5.Vector y Busqueda.py
+print("-------------------------------------------------------")
+print("Complemento5: BUSCAR ELEMENTO DENTRO DEL ARRAY.")
+print("-------------------------------------------------------")
+
+#Inicializar
+valor = []
+
+#Entradas
+print ("Ingrese número de elementos del arreglo")
+m = int( input())
+print ("Ingrese los elementos del arreglo")
+for i in range(m) :
+elemento = int( input("Ingrese Elemento: "))
+valor.append(elemento)
+
+#Búsqueda del valor dentro del arreglo
+print ("Ingrese el valor buscado: ")
+bus = int( input())
+print ("La posición del valor buscado será:")
+for i in range(m) :
+if valor[i] == bus :
+r = i
+print ("La posición del elemento es", r+1)
+break
