8464b493bd70c4e5ae922878bc43a5f25857aded
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-11 14:10:40 -0300

    Add files via upload

---
+++ f01.C9.Jugando con animales.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#extraido del libro Algoritmos resueltos con Python | p249-250
+#nombre: f01.C9.Jugando con animales.py
+print("-------------------------------------------------------")
+print("Complemento9: JUGANDO CON NOMBRES DE ANIMALES.")
+print("-------------------------------------------------------")
+
+#Inicializar
+animal = []
+
+#Entradas
+print ("Ingresar dimensión del array: ")
+tamArray = int( input())
+print ( "Ingrese los nombres de los animales:")
+for x in range(tamArray):
+elemento = input( "Ingrese Animal{}: ".format(x+1) )
+animal.append(elemento)Algoritmos resueltos con Python ISBN: 978-958-53018-2-5 250 / 257
+print ( "Ingrese animal buscado:")
+nom = input()
+print ( "El animal tiene como vecino a:")
+print("-------------------------------------------------------")
+for x in range(tamArray):
+if animal[x] == nom:
+if x == 0:
+
+#Como es Primero: No tiene vecino Izquierdo
+#Imprimir solo el derecho
+print(animal[x+1])
+elif x == tamArray-1:
+#Como es Ultimo: No tiene vecino Derecho
+
+#Imprimir solo el Izquierdo
+print(animal[x-1])
+else:
+
+#Imprimir Izquierda y derecha
+print(animal[x-1])
+print(animal[x+1])
+
