7c2efac6202c6e7f6912377c50bc653df83e65c6
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-12 12:14:15 -0300

    Add files via upload

--- a01.Calcular Distancia.py
+++ a01.Calcular Distancia.py
@@ -4,12 +4,15 @@
 print("-------------------------------------------------------")
 print("Ejercicio1: CALCULAR DISTANCIA.")
 print("-------------------------------------------------------")
+
 #Entradas
 print("Ingrese la velocidad y el tiempo de la unidad movil")
 #convirtiendo entrada a Entero
 V = float( input("Velocidad: ") )
 T = int( input("Tiempo: ") )
+
 #Proceso
 D = V*T
+
 #Salida
 print(D)
