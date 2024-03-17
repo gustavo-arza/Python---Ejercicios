34e6c61da2ae66ef8a4d64bfb6e95b1885e6d836
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-16 22:29:07 -0300

    Add files via upload

--- f01.C9.Jugando con animales.py
+++ f01.C9.Jugando con animales.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
+# -*- Scoding: utf-8 -*-
 #extraido del libro Algoritmos resueltos con Python | p249-250
-#nombre: f01.C9.Jugando con animales.py
+#nombre: f01SS.C9.Jugando con animales.py
 print("-------------------------------------------------------")
 print("Complemento9: JUGANDO CON NOMBRES DE ANIMALES.")
 print("-------------------------------------------------------")
@@ -9,31 +9,27 @@ print("-------------------------------------------------------")
 animal = []
 
 #Entradas
-print ("Ingresar dimensión del array: ")
+print ("Ingresar Sdimension del array: ")
 tamArray = int( input())
 print ( "Ingrese los nombres de los animales:")
 for x in range(tamArray):
-elemento = input( "Ingrese Animal{}: ".format(x+1) )
-animal.append(elemento)Algoritmos resueltos con Python ISBN: 978-958-53018-2-5 250 / 257
+    elemento = input( "Ingrese Animal{}: ".format(x+1) )
+animal.append(elemento)
 print ( "Ingrese animal buscado:")
 nom = input()
 print ( "El animal tiene como vecino a:")
 print("-------------------------------------------------------")
 for x in range(tamArray):
-if animal[x] == nom:
-if x == 0:
-
-#Como es Primero: No tiene vecino Izquierdo
-#Imprimir solo el derecho
-print(animal[x+1])
-elif x == tamArray-1:
-#Como es Ultimo: No tiene vecino Derecho
-
-#Imprimir solo el Izquierdo
-print(animal[x-1])
-else:
-
-#Imprimir Izquierda y derecha
-print(animal[x-1])
-print(animal[x+1])
-
+    if animal[x] == nom:
+        if x == 0:
+            #Como es Primero: No tiene vecino Izquierdo
+            #Imprimir solo el derecho
+            print(animal[x+1])
+        elif x == tamArray-1:
+            #Como es Ultimo: No tiene vecino Derecho
+            #Imprimir solo el Izquierdo
+            print(animal[x-1])
+        else:
+            #Imprimir Izquierda y derecha
+            print(animal[x-1])
+            print(animal[x+1])
