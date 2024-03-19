549cef308e5eb55e56ac084bb7c241c47831fda6
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-19 10:47:24 -0300

    Add files via upload

--- 03.Puntaje Final.py
+++ 03.Puntaje Final.py
@@ -4,17 +4,20 @@
 print("-------------------------------------------------------")
 print("Ejercicio3: PUNTAJE FINAL.")
 print("-------------------------------------------------------")
+
 #Entradas
-print("Ingrese número de respuestas correctas: ")
+print("Ingrese numero de respuestas correctas: [+3]")
 RC = int( input())
-print("Ingrese número de respuestas incorrectas: ")
+print("Ingrese numero de respuestas incorrectas: [-1]")
 RI = int( input())
-print("Ingrese número de respuestas en blanco: ")
+print("Ingrese numero de respuestas en blanco: [0]")
 RB = int( input())
+
 #Proceso
 PCR = RC*3
 PRI = RI*-1
 PRB = RB*0
 PF = PCR + PRI + PRB
+
 #Salida
 print("El puntaje total es:", PF)
