964ddd4a1623e96fe8a6fa726c81c31e4c4a858a
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-22 22:00:15 -0300

    Add files via upload

--- 07.Promedio2Numeros.py
+++ 07.Promedio2Numeros.py
@@ -2,15 +2,18 @@
 #extraido del libro Algoritmos resueltos con Python | p72-73
 #Nombre: 7.Promedio2Numeros.py
 print("-------------------------------------------------------")
-print("Ejercicio7: PROMEDIO DE DOS NÚMEROS.")
+print("Ejercicio7: PROMEDIO DE DOS NUMEROS")
 print("-------------------------------------------------------")
+
 #Entradas
 print("Ingrese notas: ")
 N1 = int( input("N1: "))
 N2 = int( input("N2: "))
+
 #Proceso
 S = N1 + N2
 P = S/2
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
