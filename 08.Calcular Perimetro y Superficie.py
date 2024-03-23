682d6fef60a77f1693b791304e909f7805c00a2e
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-22 22:00:27 -0300

    Add files via upload

--- 08.Calcular Perimetro y Superficie.py
+++ 08.Calcular Perimetro y Superficie.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 #extraido del libro Algoritmos resueltos con Python | p74-75
 #Nombre: 8.Calcular Perimetro y Superficie.py
-print("-------------------------------------------------------")
-print("Ejercicio8: CALCULAR PERÍMETRO Y SUPERFICIE DEL RECTÁNGU
-LO")
-print("-------------------------------------------------------")
+print("----------------------------------------------------------")
+print("Ejercicio8: CALCULAR PERIMETRO Y SUPERFICIE DEL RECTANGULO")
+print("----------------------------------------------------------")
+
 #Entradas
 print("Ingrese Base y Alto: ")
 BASE = float( input("Base: "))
 ALTO = float( input("Alto: "))
+
 #Proceso
 SUP = BASE*ALTO
 PER = 2*(BASE + ALTO)
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
 print("Superficie:", SUP)
-print("Perímetro:", PER)
+print("Perimetro:", PER)
