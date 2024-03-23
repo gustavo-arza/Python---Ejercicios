164cfbd90371a8f87aa8d23028affcd1dea290ea
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-23 10:19:15 -0300

    Add files via upload

--- 10.CalcularAreaVolumen.py
+++ 10.CalcularAreaVolumen.py
@@ -2,19 +2,23 @@
 #extraido del libro Algoritmos resueltos con Python | p78-79
 #nombre:10.CalcularAreaVolumen.py
 print("-------------------------------------------------------")
-print("Ejercicio10: CALCULAR ÁREA Y VOLUMEN DEL CILINDRO.")
+print("Ejercicio10: CALCULAR ï¿½REA Y VOLUMEN DEL CILINDRO.")
 print("-------------------------------------------------------")
+
 #Constantes
 PI = 3.1416
+
 #Entradas
 print("Ingrese Radio y Alto: ")
 radio = float( input("Radio: "))
 alto = float ( input("Alto: "))
+
 #Proceso
 vol = PI * radio**2 * alto
 are = 2*PI*radio*(radio + alto)
 #Salida
+
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
 print("Volumen:", vol)
-print("área:", are)
+print("Area:", are)
