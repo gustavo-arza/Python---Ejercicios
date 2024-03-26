12b5f1502273297e796bf821bb33825f11e0e9af
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-25 21:01:44 -0300

    Add files via upload

--- b03.CambiosDeSoles.py
+++ b03.CambiosDeSoles.py
@@ -2,19 +2,23 @@
 #extraido del libro Algoritmos resueltos con Python | p85
 #nombre: b03.CambiosDeSoles.py
 print("-------------------------------------------------------")
-print("Complemento3: CAMBIOS DE SOLES a EUROS y DÓLARES")
+print("Complemento3: CAMBIOS DE SOLES a EUROS y DOLARES")
 print("-------------------------------------------------------")
+
 #Constantes
 EU = 3.84
 DO = 2.28
+
 #Entradas
 print("Ingrese la cantidad de soles:")
 s = float( input())
+
 #Proceso
 d = s/DO
 e = s/EU
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
 print("En", s, "soles hay ", e, "euros")
-print("En", s, "soles hay ", d, "dólares")
+print("En", s, "soles hay ", d, "dolares")
