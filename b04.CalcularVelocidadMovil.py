1038a4fe299f9d34328b491a16403fddb7cdda66
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-26 10:40:21 -0300

    Add files via upload

--- b04.CalcularVelocidadMovil.py
+++ b04.CalcularVelocidadMovil.py
@@ -2,15 +2,18 @@
 #extraido del libro Algoritmos resueltos con Python | p87
 #nombre: b04.CalcularVelocidadMovil.py
 print("-------------------------------------------------------")
-print("Complemento4: CALCULAR LA VELOCIDAD DE UN MÓVIL.")
+print("Complemento4: CALCULAR LA VELOCIDAD DE UN MOVIL.")
 print("-------------------------------------------------------")
+
 #Entradas
 print("Ingrese el tiempo en segundos:")
 t = float( input())
 print("Ingrese la distancia en metros:")
 d = float( input())
+
 #Proceso
 v = d/t
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
