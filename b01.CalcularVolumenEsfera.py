e50269d2d7bbc55de8eeab26079dbee487567280
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-25 21:01:00 -0300

    Add files via upload

--- b01.CalcularVolumenEsfera.py
+++ b01.CalcularVolumenEsfera.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 #extraido del libro Algoritmos resueltos con Python | p81
 #nombre: b01.CalcularVolumenEsfera.py
+
 print("-------------------------------------------------------")
 print("Complemento1: CALCULAR VOLUMEN DE LA ESFERA.")
 print("-------------------------------------------------------")
+
 #Constantes
 PI = 3.1416
+
 #Entradas
 r = float( input("Ingrese Radio: "))
+
 #Proceso
 v = 4/3 * PI * r**3
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
