ae26adfc192f95f41035ed0c1429054177bcecf8
Gustavo Arza <95915763+gustavo-arza@users.noreply.github.com>
2024-03-29 09:51:14 -0300

    Add files via upload

--- b08.TiempoDeEncuentro.py
+++ b08.TiempoDeEncuentro.py
@@ -4,14 +4,17 @@
 print("-------------------------------------------------------")
 print("Complemento8: TIEMPO DE ENCUENTRO.")
 print("-------------------------------------------------------")
+
 #Entradas
 print("Ingrese las velocidades:")
 va = float( input("Va: "))
 vb = float( input("Vb: "))
 print("Ingrese la distancia que los separa:")
 D = float( input())
+
 #Proceso
 te = D/(va+vb)
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
